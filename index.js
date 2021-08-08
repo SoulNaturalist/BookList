4f1fa9ce962fb24a62abb35b1a5c2769d93a70b2
Saywex89 <omg12121@mail.ru>
2021-08-09 01:35:24 +0700

    add routers and templates html/css files

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const express = require("express");
 
+const app = express();
+
+app.get("/main", function(request, response){
+    response.sendFile(__dirname + "/HomePage/index.html")
+});
+
+app.get("/login", function(request, response){
+    response.sendFile(__dirname + "/Login/index.html")
+});
+
+app.get("/registration", function(request, response){
+    response.sendFile(__dirname + "/Registration/index.html")
+});
+
+app.get("/mybooks", function(request, response){
+    response.sendFile(__dirname + "/UserBookList/index.html")
+});
+
+app.get("/addbook", function(request, response){
+    response.sendFile(__dirname + "/AddBookForm/index.html")
+});
+
+
+app.listen(3000);
+console.log('App Starting!')

2847d237b3d4a30fd459b7b22ed96141c5f7fc8d
Saywex89 <omg12121@mail.ru>
2021-08-26 12:32:28 +0700

    add api methods

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
-
+const http = require("http");
 const app = express();
+const fs = require("fs");
+const filePath = "users.json";
+const passwordHash = require('password-hash');
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json()) 
 
 app.get("/main", function(request, response){
     response.sendFile(__dirname + "/HomePage/index.html")
@@ -14,6 +20,24 @@ app.get("/registration", function(request, response){
     response.sendFile(__dirname + "/Registration/index.html")
 });
 
+app.post("/registration", function (request, response) {
+    // let username = request.body.username;
+    // let password = request.body.user_password;
+    // let hashedPassword = passwordHash.generate(password);
+    // let data = fs.readFileSync(filePath, "utf8");
+    // let users = JSON.parse(data);
+    // let UserTemplate = {
+    //     "id":2,
+    //     "name":username,
+    //     "password":hashedPassword,
+    //     "books": []
+    // }
+    // UserTemplate = JSON.stringify(UserTemplate);
+    // users.push(`,${UserTemplate}`);
+    // data = JSON.stringify(users);
+    // fs.writeFileSync("users.json", data);
+});
+
 app.get("/mybooks", function(request, response){
     response.sendFile(__dirname + "/UserBookList/index.html")
 });
@@ -22,6 +46,21 @@ app.get("/addbook", function(request, response){
     response.sendFile(__dirname + "/AddBookForm/index.html")
 });
 
+app.get("/api/users", function(req, res){
+    const content = fs.readFileSync(filePath,"utf8");
+    const users = JSON.parse(content);
+    res.send(users);
+});
 
+app.get("/api/users/:id", function(req, res){
+    const id = req.params.id;
+    const content = fs.readFileSync(filePath, "utf8");
+    const users = JSON.parse(content);
+    for (let user of users) {
+        if (Number(user.id) === Number(id)) {
+            res.send(user);
+        }
+    }
+})
 app.listen(3000);
 console.log('App Starting!')

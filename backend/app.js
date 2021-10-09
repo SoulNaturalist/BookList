7e0e2dacfbfc06249f966856bb4dd39b264a9842
Saywex89 <omg12121@mail.ru>
2021-10-10 06:02:40 +0700

    add api endpoints

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,51 @@
 const express = require('express')
 const mongoose = require('mongoose');
-const { CONNECTION_URL } = require('./config.js');
+const jwt = require('jsonwebtoken');
+const { CONNECTION_URL, PORT } = require('./config.js');
 const app = express()
- 
+
+
+const urlencodedParser = express.urlencoded({extended: false});
 
 mongoose.connect(CONNECTION_URL)
 .then(() => {
-    console.log('Connected');
+    console.log('Database connected');
 })
-.catch( (err) => {
-    console.log('Connection failed '+ err.message);
+.catch((err) => {
+    console.log(`Connection failed ${err.message}`);
 });
 
-app.get('/api/get_book/:book', function (req, res) {
-    let bookName = req.params.book; 
-    console.log(bookName)
-})
+app.get('/api/user/:id', function (req, res) {
+    let userId = req.params.book; 
+    console.log(userId)
+});
+
+
+app.post('/api/add_book', urlencodedParser, function (req, res) {
+    let valideBook = false;
+    let valideAuthor = false;
+    let valideEstimation = false;
+    if(!req.body) return response.sendStatus(400);
+    if (req.body['book']) {
+        valideBook = true;
+    } else {
+        return res.status(400).send('Ошибка - Название книги обязательный параметр.');
+    }
+    if (req.body['author']) {
+        valideAuthor = true;
+    } else {
+        return res.status(400).send('Ошибка - Автор книги обязательный параметр.');
+    }
+    if (req.body['estimation']) {
+        valideEstimation = true;
+    } else {
+        return res.status(400).send('Ошибка - Оценка книги обязательный параметр.');
+    }
+    
+    if (valideBook && valideAuthor && valideEstimation) {
+        return res.sendStatus(200);
+    }
+});
+
  
-app.listen(3000)
+app.listen(PORT, () => console.log(`Server start port - ${PORT}`))
--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 
-const CONNECTION_URL = "mongodb+srv://:@bdfirst.mskcn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const CONNECTION_URL = "mongodb+srv://@bdfirst.mskcn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = 3000;
 
-
-module.exports = { CONNECTION_URL };
+module.exports = { CONNECTION_URL,PORT };

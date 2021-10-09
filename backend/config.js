49609b2073297f009dfc1873dd563357589ff3d3
Saywex89 <omg12121@mail.ru>
2021-10-10 06:11:43 +0700

    remove default css and js in react app

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 
 const CONNECTION_URL = "mongodb+srv://@bdfirst.mskcn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const PORT = 3000;
+const PORT = 3000
 
-module.exports = { CONNECTION_URL,PORT };
+module.exports = { CONNECTION_URL, PORT };

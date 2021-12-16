c04841a17d670f6f67a0eaa5773dcdbda218aa4a
ResponseGood <omg12121@mail.ru>
2021-12-17 04:59:22 +0700

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
+const cors = require('cors')
 const  api = require('./api.js');
 const { PORT } = require('./config');
 const express = require('express');
 const app = express();
 app.use(express.json());  
 app.use('/', api);
+app.use(cors());
 
 app.listen(PORT, () => console.log(`Server start port - ${PORT}`));

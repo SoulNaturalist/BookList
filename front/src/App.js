76123989425207081f488802db5620b676af661b
ResponseGood <omg12121@mail.ru>
2021-12-17 04:58:31 +0700

    add description site, deleted default comments

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      
+  //const [Auth, SetAuth] = React.useState(true);
+  //const [Loading, SetLoading] = React.useState(true);
+  React.useEffect(() => {
+    axios({method: 'post',url:'http://127.0.0.1:3030/api/check_auth', headers: {}})
+    .then(res => {
+      console.log(res);
+    }).catch(err => {
+      console.log(err);
+    })
+  }, [])
+
+
+return (
+  <div className="App">
+    <div className="cointainer">
+      <p className="description_title">Book List - это сайт на котором ты можешь указать 
+      какие книги прочитал и как их оцениваешь.</p>
     </div>
-  );
-}
+  </div>
+
+)}
 
 export default App;

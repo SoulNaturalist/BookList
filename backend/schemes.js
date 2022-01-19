afcde78d0456d726cc0cd1b3a708b65c5e27b94a
ResponseGood <omg12121@mail.ru>
2022-01-19 21:56:27 +0300

    add review schema

--- backend/schemes.js
+++ backend/schemes.js
@@ -8,15 +8,17 @@ const UserSchema = new Schema({
     registration_date:{ type:String, default: new Date().toLocaleDateString('pt-PT')},
     book_read_count: {type: Number, default:0},
     books: { type: Object, default:{} },
+    status:{ type: Object, default:{} },
     role: {type: Boolean,default: false},
 }, { minimize: false });
 
-const BookShema = new Schema ({
+const BookSchema = new Schema ({
     book_name: { type: String, required: true},
     book_author: {type: String, required: true},
     year_of_release: {type: Number, required: true},
     description: {type: String, required: true, default:"description default"},
-})
+    reviews: {type: Object, default:{}},
+}, { minimize: false })
 
 const AuthorSchema = new Schema({
     author_name: {type: String, required: true},
@@ -26,4 +28,12 @@ const AuthorSchema = new Schema({
     description: {type: String, required: false, default:"This is a temporary description, the author's descriptions will appear soon."},
 })
 
-module.exports = {UserSchema,BookShema,AuthorSchema};
+
+const Review = new Schema({
+    book_id: { type: Number, required: true},
+    author_review: { type: String, required: true},
+    text_review: {type: String, required: true},
+    rating_view: {type: Number, required: true}
+})
+
+module.exports = {UserSchema,BookSchema,AuthorSchema};

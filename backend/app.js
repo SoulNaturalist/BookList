571d124781f133d0d3cb275d31153ad5fecd0f1f
ResponseGood <omg12121@mail.ru>
2022-01-22 05:14:28 +0300

    used telnet to protect against vulnerabilities

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const cors = require('cors')
 const  api = require('./api.js');
 const { PORT } = require('./config');
+const helmet = require('helmet');
 const express = require('express');
 const app = express();
 app.use(express.json());  
 app.use('/', api);
 app.use(cors());
+app.use(helmet());
 
 app.listen(PORT, () => console.log(`Server start port - ${PORT}`));

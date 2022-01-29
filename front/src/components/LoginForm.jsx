d02d5479d1c0e660c40f5267b6be69403506875a
ResponseGood <omg12121@mail.ru>
2022-01-29 11:06:35 +0300

    fixed routing

--- front/src/components/LoginForm.jsx
+++ front/src/components/LoginForm.jsx
@@ -4,38 +4,53 @@ import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 
 function LoginForm () {
+    const [User, setUser] = React.useState([]);
+    const [Error,setError] = React.useState(false);
+    const navigate = useNavigate();
+    React.useEffect(() => {
+        axios('http://127.0.0.1:3030/api/auth',{method: 'post',mode:'no-cors',withCredentials: true})
+        .then(res => {setUser(res.data)})
+        .catch(err => {setError(err)})
+    }, [])
+    const CheckAuth = () => {
+        if (User.auth_data) {
+            return navigate("/profile")
+        } else {
+            return <div>
+                <h2 className="auth_title">Авторизация</h2>
+                <div className="form_wrapper">
+                    <form onSubmit={handleSubmit(onSubmit)}>
+
+                        <input className="login"
+                        placeholder="Логин"
+                        required
+                        {...register("login", {})}
+                        />
+                        {errors.login && <p>{errors.login.message}</p>}
+                        <input className="password"
+                        placeholder="Пароль"
+                        {...register("password", {})}
+                        type="password" required/>
+                        {errors.password && <p>{errors.password.message}</p>}
+                        <input type="submit" value="Вход" />
+                    </form>
+                </div>
+            </div>
+        }
+
+    }
     const {register, formState: { errors }, handleSubmit } = useForm({
         mode: "onChange"
     });
-    const navigate = useNavigate();
-    const onSubmit = async (data) => {
+    const onSubmit = (data) => {
         axios({method: 'post',url:'http://127.0.0.1:3030/api/login/',withCredentials: true, headers: {}, data: {email: data.login, password: data.password}})
-        .then(response => {console.log(response.data)})
+        .then(response => {navigate('/profile')})
         .catch(error => {console.log(error)})
-        navigate('/');
     }
     return (
         <div>
-            <h2 className="auth_title">Авторизация</h2>
-            <div className="form_wrapper">
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <input className="login"
-                    placeholder="Логин"
-                    required
-                    {...register("login", {})}
-                    />
-                    {errors.login && <p>{errors.login.message}</p>}
-                    <input className="password"
-                    placeholder="Пароль"
-                    {...register("password", {})}
-                    type="password" required/>
-                    {errors.password && <p>{errors.password.message}</p>}
-                    <input type="submit" value="Вход" />
-                </form>
-            </div>
-        
+            {CheckAuth()}
         </div>
-  
 )}
 
 export default LoginForm;

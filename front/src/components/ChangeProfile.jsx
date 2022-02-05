5f2dc9c04367b97803f7b26ce9c31c29b24f450f
ResponseGood <omg12121@mail.ru>
2022-02-05 12:36:45 +0300

    removed unused import

--- front/src/components/ChangeProfile.jsx
+++ front/src/components/ChangeProfile.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import axios from 'axios';
-import {useNavigate} from "react-router-dom";
 import ButtonUnstyled, { buttonUnstyledClasses } from '@mui/base/ButtonUnstyled';
 import { styled } from '@mui/system';
 
@@ -8,7 +7,6 @@ function ChangeProfile () {
     
     return (
         <div>
-            {CheckAuth()}
         </div>
   
 )}

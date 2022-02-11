4f34e3d6bd8a19d62264b620c61543a11b55e91e
ResponseGood <omg12121@mail.ru>
2022-02-11 09:40:28 +0300

    fixed css Book Page component

--- front/src/components/BookPage.jsx
+++ front/src/components/BookPage.jsx
@@ -16,12 +16,29 @@ function BookPage () {
     .catch(err => {setError(err)})
 
   }, [])
+  
+  function addBook(status) {
+    let formatBook = { ...Book };
+    delete formatBook.reviews;
+    delete formatBook.slug;
+    delete formatBook.cover;
+    axios({method: 'post',url:`http://127.0.0.1:3030/api/add_book`,withCredentials: true, headers: {},data: {
+      book_name:formatBook.book_name,book_author:formatBook.book_author,year_of_release:formatBook.year_of_release,
+      book_status:status
+    }})
+    .then(response => {
+      console.log(response)
+    })
+    .catch(err => {console.log(err)})
+  }
   return <div>
-    <img className="book_cover" src={Book.cover}/>
+    <h3 className="title_book">{Book.book_name} {Book.book_author}</h3>
+    <p className="description_book">{Book.description}</p>
+    <img className="book_cover" src={Book.cover} alt="cover"/>
     <div className="btn-group">
-      <button>Прочитана</button>
-      <button>Запланирована</button>
-      <button>Брошена</button>
+      <button onClick={() => addBook("readed")}>Прочитана</button>
+      <button onClick={() => addBook("planned")}>Запланирована</button>
+      <button onClick={() => addBook("abandoned")}>Брошена</button>
     </div>
   </div>
 }

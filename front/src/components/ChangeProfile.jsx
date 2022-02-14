20c8bb48e92f8d5a0625aa96c9b32570a80858b4
ResponseGood <omg12121@mail.ru>
2022-02-14 13:08:11 +0300

    Added the ability to change the profile

--- front/src/components/ChangeProfile.jsx
+++ front/src/components/ChangeProfile.jsx
@@ -1,26 +1,59 @@
 import React from 'react';
 import axios from 'axios';
-import { styled } from '@mui/system';
 import {useNavigate} from "react-router-dom";
-import ButtonUnstyled, { buttonUnstyledClasses } from '@mui/base/ButtonUnstyled';
+import { useForm } from "react-hook-form";
 
 function ChangeProfile () {
     const [Data,setData] = React.useState("");
     const [Error,setError] = React.useState(false);
+    const [Username, setUsername] = React.useState("");
     const navigate = useNavigate("/");
     React.useEffect(() => {
         axios({method: 'post',url:'http://127.0.0.1:3030/api/auth',withCredentials: true, headers: {}})
         .then(response => {setData(response.data)})
         .catch(err => {setError(err)})
     }, [])
+    const {register,handleSubmit} = useForm();
     const CheckAuth = () => {
-        if (Data.auth_data && !Error) {
+        if (Data.auth_data) {
+            return (
+                <div>
+                    <h2 className="title_main">Основное</h2>
+                    <div className="form_wrapper">
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <input className="nicname_input"
+                            placeholder="Никнейм"
+                            defaultValue={Data.auth_data.username}
+                            {...register("nicname", {})}
+                            />
+                            <input type="text" className="status_input"
+                            placeholder="Статус"
+                            defaultValue={Data.auth_data.status}
+                            {...register("status", {})}
+                            />
+                            <input type="text" className="avatar_input"
+                            placeholder="Аватар"
+                            defaultValue={Data.auth_data.avatar}
+                            {...register("avatar", {})}
+                            />
+                            <input className="save_button" type="submit" value="Сохранить" />
+                        </form>
+                    </div>
+                </div>
+        )
 
         } else {
             navigate("/login")
         }
     }
-
+    const onSubmit = (data) => {
+        if (data.nicname && data.status && data.avatar) {
+            axios({method: 'put',url:'http://127.0.0.1:3030/api/setting_user/',withCredentials: true, headers: {}, data: 
+            {username:data.nicname, status:data.status,avatar:data.avatar}})
+            .then(response => {navigate('/profile')})
+            .catch(error => {console.log(error)})
+        }
+    }
     return (
         <div>
             {CheckAuth()}

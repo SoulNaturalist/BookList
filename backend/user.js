9f1552c4bb44a6918b918809bb40b1d5394d8c8f
ResponseGood <omg12121@mail.ru>
2022-07-04 08:15:30 +0300

    fixed some bugs

--- backend/user.js
+++ backend/user.js
@@ -75,46 +75,41 @@ router.post('/api/change_passwd', async function (req, res) {
     try {
         const password = req.body["password"];
         const newPassword = req.body["new_password"];
-        if (password) {
-            const data = jwt.verify(token, JWT_PRIVATE_TOKEN);
-            const Users = DB.model('users', UserSchema);
-            const userData = await Users.findOne({_id: data['data']}).exec();
-            const passwordSuccessMatch = await bcrypt.compare(password, userData.password);
-            if (passwordSuccessMatch && Boolean(userData)) {
-                // Password checked and user have auth
-                if (userData.twoAuth) {
-                    // user enabled 2 factor auth
-                    const codeConfirmPassword = uuid.v4();
-                    const changedCode = await Users.updateOne({_id: userData['_id']}, { $set: {code:codeConfirmPassword}}).exec();
-                    const transporter = nodemailer.createTransport({
-                        service: "Outlook365",
-                        host: "smtp.office365.com",
-                        port: "587",
-                        tls: {ciphers: "SSLv3",rejectUnauthorized: false,},
-                        auth: {
-                            user: outlookLogin, 
-                            pass: outlookPass
-                        }
-                      });
-                      const mailOptions = {from: outlookLogin,to:userData.email,
-                        subject: "Смена пароля BookList",
-                        html: `<h1>Здравствуй ${userData.username},кто-то попытался сменить твой пароль учетной записи.</h1>\n
-                        <p>Если это был не ты, проигнорируй это письмо.</p>
-                        <b><a href='http://127.0.0.1:3000/change_password/${codeConfirmPassword}'>Сменить пароль</a></b>
-                        `
-                      }
-                      const messageSendData = await transporter.sendMail(mailOptions)
-                      return messageSendData && changedCode.modifiedCount  ? res.sendStatus(200):res.sendStatus(500);
-                } else {
-                    if (password && newPassword) {
-                        const hashPassword = await bcrypt.hash(newPassword, 10);
-                        const changePassword = await Users.updateOne({_id: data['data']}, { $set: {password:hashPassword}}).exec();
-                        return changePassword && modifiedCount ? res.sendStatus(201):res.sendStatus(401);
-                    }
-                }
+        const data = jwt.verify(token, JWT_PRIVATE_TOKEN);
+        const Users = DB.model('users', UserSchema);
+        const userData = await Users.findOne({_id: data['data']}).exec();
+        if (Boolean(password) && Boolean(newPassword) && password !== newPassword) {
+            const passwordSuccessMatch = await bcrypt.compare(password,userData.password);
+            if (passwordSuccessMatch) {
+                const hashNewPassword = await bcrypt.hash(newPassword, 10);
+                const changePassword = await Users.updateOne({_id: data['data']}, { $set: {password:hashNewPassword}}).exec();
+                return changePassword.modifiedCount ? res.sendStatus(201):res.sendStatus(401);
             } else {
                 return res.sendStatus(400);
-
+            }
+        } else {
+            if (userData.twoAuth && Boolean(userData)) {
+                // user enabled 2 factor auth
+                const codeConfirmPassword = uuid.v4();
+                const changedCode = await Users.updateOne({_id: userData['_id']}, { $set: {code:codeConfirmPassword}}).exec();
+                const transporter = nodemailer.createTransport({
+                    host: 'smtp.mail.ru',
+                    port: 465,
+                    secure: true, 
+                    auth: {
+                        user: outlookLogin,
+                        pass: outlookPass
+                    }
+                });
+                const mailOptions = {from: outlookLogin,to:userData.email,
+                    subject: "Смена пароля BookList",
+                    html: `<h1>Здравствуй ${userData.username},кто-то попытался сменить твой пароль учетной записи.</h1>\n
+                    <p>Если это был не ты, проигнорируй это письмо.</p>
+                    <p>Код подтверждения${codeConfirmPassword}</p>
+                    `
+                }
+                const messageSendData = await transporter.sendMail(mailOptions);
+                return messageSendData && changedCode.modifiedCount  ? res.sendStatus(200):res.sendStatus(500);
             }
         }
     } catch (e) {
@@ -214,51 +209,25 @@ router.post('/api/confirm_email', async function(req, res) {
     };
     const data = await Users.findOne({code:codeCheck},Query).exec();
     if (data) {
-        await Users.updateOne({code: codeCheck}, { $set: {emailConfirm:true}})
-        res.status(200).json({'message':'Почта потверждена!'})
+        const changeEmail = await Users.updateOne({code: codeCheck}, { $set: {emailConfirm:true}})
+        return changeEmail.modifiedCount ? res.json({'message':'Почта потверждена!'}):res.json({'message':'Произошла ошибка'});
     }
     
 })
 
 
-router.post('/api/validate_password_reset/:code', async function(req, res) {
+router.post('/api/confirm_change_password', async function(req, res) {
     const token = req.cookies.JWT;
     try {
-        const passwordCode = req.params.code;
         const data = jwt.verify(token, JWT_PRIVATE_TOKEN);
         const users = DB.model('users', UserSchema);
-        const Query = { 
-            __v: false,
-            _id:false,
-            password: false
-        };
-        const dataUser = await users.findOne({_id: data['data']},Query).exec();
-        if (passwordCode) {
-            const checkUserData = await users.findOne({code: passwordCode},Query).exec();
-            if (checkUserData['username'] === dataUser['username']) {
-                return res.sendStatus(200);
-            } else {
-                return res.sendStatus(400);
-            }
-        }
-    } catch (e) {
-        console.log(e);
-        return res.sendStatus(403);
-    }
-})
-
-router.post('/api/change_password_2auth', async function(req, res) {
-    const token = req.cookies.JWT;
-    try {
-        const data = jwt.verify(token, JWT_PRIVATE_TOKEN);
-        const dataUser = await users.findOne({_id: data['data']},Query).exec();
+        const dataUser = await users.findOne({_id: data['data']}).exec();
         const newPassword = req.body["new_password"];
         const codeFactor = req.body["code"];
         const hashedPassword = await bcrypt.hash(newPassword, 10);
         if (dataUser['password'] !== hashedPassword && dataUser['code'] === codeFactor) {
-            const users = DB.model('users', UserSchema);
             const userPasswordChange = await users.updateOne({username: dataUser['username']}, { $set: {password:hashedPassword}}).exec();
-            const changedCode = await Users.updateOne({_id: dataUser['_id']}, { $set: {code:uuid.v4()}}).exec();
+            const changedCode = await users.updateOne({_id: dataUser['_id']}, { $set: {code:uuid.v4()}}).exec();
             return userPasswordChange.modifiedCount && changedCode.modifiedCount ? res.sendStatus(200):res.sendStatus(400);
         }
     } catch (e) {

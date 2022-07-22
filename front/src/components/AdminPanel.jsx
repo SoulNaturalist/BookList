01a79e9e7d28de82583136ce27e405daf8242381
ResponseGood <omg12121@mail.ru>
2022-07-22 03:49:58 +0300

    writed alt to img buttons

--- front/src/components/AdminPanel.jsx
+++ front/src/components/AdminPanel.jsx
@@ -34,19 +34,19 @@ function AdminPanel () {
                     <div className="admin_manage_table">
                         <div className="manage_buttons">
                             <div>
-                                <img src="https://cdn-icons-png.flaticon.com/512/456/456212.png" width="100px" height="100px" alt="user" onClick={() => alert(dataInput.userValue)}/>
+                                <img src="https://cdn-icons-png.flaticon.com/512/456/456212.png" width="100px" height="100px" alt="user_icon" onClick={() => alert(dataInput.userValue)}/>
                                 <input onChange={(e) => setDataInput({...dataInput,userValue:e.target.value})}></input>
                             </div>
                             <div>
-                                <img src="https://cdn-icons-png.flaticon.com/512/709/709489.png" width="100px" height="100px" alt="ban" onClick={() => alert(dataInput.banValue)}/>
+                                <img src="https://cdn-icons-png.flaticon.com/512/709/709489.png" width="100px" height="100px" alt="ban_icon" onClick={() => alert(dataInput.banValue)}/>
                                 <input onChange={(e) => setDataInput({...dataInput,banValue:e.target.value})}></input>
                             </div>
                             <div>
-                                <img src="https://cdn-icons-png.flaticon.com/512/1933/1933005.png" width="100px" height="100px" alt="send" onClick={() => alert(dataInput.sendValue)}/>
+                                <img src="https://cdn-icons-png.flaticon.com/512/1933/1933005.png" width="100px" height="100px" alt="send_icon" onClick={() => alert(dataInput.sendValue)}/>
                                 <input onChange={(e) => setDataInput({...dataInput,sendValue:e.target.value})}></input>
                             </div>
                             <div>
-                                <img src="https://cdn-icons.flaticon.com/png/512/657/premium/657059.png?token=exp=1658448149~hmac=de4079ef2937f8cf864e1110348ed5e8" width="100px" height="100px" alt="delete" onClick={() => alert(dataInput.deleteValue)}/>
+                                <img src="https://cdn-icons.flaticon.com/png/512/657/premium/657059.png?token=exp=1658448149~hmac=de4079ef2937f8cf864e1110348ed5e8" width="100px" height="100px" alt="delete_icon" onClick={() => alert(dataInput.deleteValue)}/>
                                 <input onChange={(e) => setDataInput({...dataInput,deleteValue:e.target.value})}></input>
                             </div>
                         </div>

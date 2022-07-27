488a38bf8c98174bf591c394ff06513b27d74056
ResponseGood <omg12121@mail.ru>
2022-07-27 13:32:14 +0300

    removed deprecated & unused code / fixed some bugs in Header component

--- front/src/components/AdminPanel.jsx
+++ front/src/components/AdminPanel.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 import CircularProgress from '@mui/material/CircularProgress';
 
 
--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {BrowserRouter as Router} from "react-router-dom";
 import axios from 'axios';
 import logo from '../assets/logo.png';
 
@@ -11,7 +10,7 @@ function Header() {
         .catch((err) => console.log(err))
     }, [])
     const ProfileElement = () => {
-        if (Data.auth_data) {
+        if (Data.auth_data && window.location.href !== "http://localhost:3000/logout") {
             return <p className="profile"><a href={`/user/${Data.auth_data.username}`}>Профиль</a></p>
         } else {
             return <p className="log-in"><a href="/login">Войти</a></p>
@@ -23,11 +22,9 @@ function Header() {
                 <nav>
                     <ul>
                         <li>
-                            <Router>
-                                <img className="logo" src={logo} alt="logo"/>
-                                <h1><a href="/">BookList</a></h1>
-                                {ProfileElement()}
-                            </Router>
+                            <img className="logo" src={logo} alt="logo"/>
+                            <h1><a href="/">BookList</a></h1>
+                            {ProfileElement()}
                         </li>
                     </ul>
                 </nav>

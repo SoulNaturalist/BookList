2387789a2b495b237069b5045d7e1456418858ab
ResponseGood <omg12121@mail.ru>
2022-07-28 20:22:02 +0300

    rewritten search in Catalog

--- front/src/components/BooksCatalog.jsx
+++ front/src/components/BooksCatalog.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import axios from "axios";
-import { styled } from '@mui/system';
 import CircularProgress from '@mui/material/CircularProgress';
-import ButtonUnstyled, { buttonUnstyledClasses } from '@mui/base/ButtonUnstyled';
 
 
 function BooksCatalog () {
@@ -11,46 +9,11 @@ function BooksCatalog () {
     const [Error,setError] = React.useState(false);
     const [Search,setSearch] = React.useState(false);
     const [searchButton,setButton] = React.useState(false);
-    const blue = {
-        500: '#59ff91',
-        600: '#47d677',
-        700: '#474646',
-    };
-    const CustomButtonRoot = styled('span')`
-    font-family: IBM Plex Sans, sans-serif;
-    font-weight: bold;
-    font-size: 0.875rem;
-    background-color: ${blue[500]};
-    position:relative;
-    display: inline-block;
-    bottom:-30px;
-    right:28%;
-    padding:10px;
-    border-radius:5px;
-    color: white;
-    transition: all 150ms ease;
-    cursor: pointer;
-    border: none;
-
-    &:hover {
-        background-color: ${blue[600]};
-    }
-
-    &.${buttonUnstyledClasses.active} {
-        background-color: ${blue[700]};
-    }
-
-    &.${buttonUnstyledClasses.focusVisible} {
-        box-shadow: 0 4px 20px 0 rgba(61, 71, 82, 0.1), 0 0 0 5px rgba(0, 127, 255, 0.5);
-        outline: none;
-    }
-    `;
-    function CustomButton(props) {
-        return <ButtonUnstyled {...props} component={CustomButtonRoot} />;
-    }
     const SearchData = (e) => {
         if (e.target.value !== 'Название книги' && e.target.value !== '') {
             setSearch(e.target.value)
+            setButton(false)
+        } else if (e.target.value !== 'Название книги' && e.target.value === '') {
             setButton(false)
         }
     }
@@ -73,8 +36,8 @@ function BooksCatalog () {
         return <div>
             <h1 className="title__catalog">Каталог книг</h1>
             <div className="flex-wrapper">
-            <CustomButton onClick={getDataSearch}>Найти</CustomButton>
-            <input type="search" className="search_books" defaultValue='Название книги' onInput={e => SearchData(e)}/>  
+            <button onClick={getDataSearch} className="search_button">Найти</button>
+            <input type="search" className="search_books" defaultValue='Название книги' onChange={e => SearchData(e)}/>  
             {loading ? <div style={{display: 'flex', justifyContent: 'center', position:'relative', top:'30px'}}><CircularProgress disableShrink /></div>:
             !Boolean(searchButton) ? (
                 Books.map((book, index) => (

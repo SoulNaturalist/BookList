839a46426f4b7e480049d53ab3f920a75eb2fb62
ResponseGood <omg12121@mail.ru>
2022-07-28 16:48:13 +0300

    deleted ReviewPage

--- front/src/App.js
+++ front/src/App.js
@@ -5,7 +5,6 @@ import LoginForm from './components/LoginForm.jsx';
 import BookPage from './components/BookPage.jsx';
 import BooksCatalog from './components/BooksCatalog.jsx';
 import ChangeProfile from './components/ChangeProfile.jsx';
-import ReviewPage from './components/ReviewPage.jsx';
 import UserBooks from './components/UserBooks.jsx';
 import AdminPanel from './components/AdminPanel.jsx';
 import LogoutPage from './components/LogoutPage.jsx';
@@ -35,7 +34,6 @@ return (
           <Route path="/change_password" element={<PasswordChange/>}/>
           <Route path="/catalog" element={<BooksCatalog/>}/>
           <Route path="/book/:slug" element={<BookPage />}/>
-          <Route path="/write_review" element={<ReviewPage />}/>
           <Route path="/admin" element={<AdminPanel />}/>
           <Route path="/check_email" element={<CheckEmail/>}/>
           <Route path="/leader_board" element={<LeaderBoard/>}/>

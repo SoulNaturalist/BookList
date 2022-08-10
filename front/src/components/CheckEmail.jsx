7a810b975b3607121cbec85b757859470a169dfb
ResponseGood <omg12121@mail.ru>
2022-08-10 23:50:15 +0300

    updated email Img in Check Email component

--- front/src/components/CheckEmail.jsx
+++ front/src/components/CheckEmail.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import emailCheckImg from '../assets/undraw_Opened_re_i38e.png'
 
 function CheckEmail () {
     return <div>
         <h2 className="check_email">Проверьте почту,на неё было отправлено письмо с подтверждением.</h2>
-        <img src="https://mbudo-cdt.rf-ds.ru/wp-content/uploads/2022/03/ea8dc020c867ad07c054d087d7a5d80e.png" alt="email" className="email_icon"/>
+        <img src={emailCheckImg} alt="email" className="email_icon"/>
     </div>
 }
 export default CheckEmail;

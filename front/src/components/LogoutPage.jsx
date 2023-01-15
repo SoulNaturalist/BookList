1f4bfedd0436f23db3f09cb4e4e22cf83a904904
SoulNaturalist <omg12121@mail.ru>
2023-01-15 14:40:01 +0300

    Component LogoutPage to UseSWR has been rewritten

--- front/src/components/LogoutPage.jsx
+++ front/src/components/LogoutPage.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import axios from 'axios';
+import useSWR from 'swr';
 import Alert from '@mui/material/Alert';
 
 function LogoutPage () {
-    const [User, setUser] = React.useState([]);
-    const [Error,setError] = React.useState("");
     const [authStatus,setAuthStatus] = React.useState("");
-    React.useEffect(() => {
-        axios('http://127.0.0.1:3030/api/logout',{method: 'get',mode:'no-cors',withCredentials: true})
-        .then(res => {setAuthStatus(res.data)})
-        .catch(err => {setError(err)})
-    }, [])
+    const { data } = useSWR('http://127.0.0.1:3030/api/logout', (apiURL) => fetch(apiURL,{
+        method: 'get',headers: {'Accept': 'application/json','Content-Type': 'application/json'},credentials: 'include'
+    }).then(res => {res.json()
+        setAuthStatus(true)
+    }));
     const logoutAlert = () => {
         if (authStatus) {
             return <Alert variant="filled" style={{display: 'flex', justifyContent: 'center'}} severity="success">Вы успешно вышли из учетной записи!</Alert>

1454dda77bb13f0f94aa42732bf3fddd04d9c03a
SoulNaturalist <omg12121@mail.ru>
2023-01-17 00:22:21 +0300

    Header rewritten to styled-components

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import useSWR from 'swr';
 import React from 'react';
+import styled from 'styled-components';
 import logo from '../assets/logo.png';
 
 function Header() {
@@ -11,28 +12,52 @@ function Header() {
         },
         credentials: 'include'
     }).then(res => res.json()));
+    const Header = styled.header`
+    background-color:#FF7A00
+    `;
+    const TitleHeader = styled.header`
+    font-family: 'Manrope', sans-serif;
+    position: relative;
+    top:10px;
+    font-size:28px;
+    height:50px;
+    width:50%;
+    display: inline-block;
+    `;
+    const ImgHeader = styled.img`
+    float: left;
+    margin-left:15px;
+    margin-top:10px;
+    width:40px;
+    `
+    const ParagraphLogin = styled.p`
+    font-size:15px;
+    float:right;
+    margin-top:15px;
+    margin-right:5px;
+    `
+    const Link = styled.a`
+    color:black;
+    text-decoration: none;
+    `
     const loginComponent = () => {
         const subLink = window.location.href.split("/")[3];
         if (data && data.auth_data && subLink !== "logout") {
-            return <p className="profile"><a href={`/user/${data.auth_data.username}`}>Профиль</a></p>
+            return <ParagraphLogin><Link href={`/user/${data.auth_data.username}`}>Профиль</Link></ParagraphLogin>
         } else {
-            return <p className="log-in"><a href="/login">Войти</a></p>
+            return <ParagraphLogin><Link href="/login">Войти</Link></ParagraphLogin>
         }
     }
     return (
-        <div className="header">
-            <header>
-                <nav>
-                    <ul>
-                        <li>
-                            <img className="logo" src={logo} alt="logo"/>
-                            <h1><a href="/">BookList</a></h1>
-                            {loginComponent()}
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-        </div>
+        <Header>
+            <Link href="/">
+                <ImgHeader src={logo}/>
+                <TitleHeader>
+                    BookList
+                </TitleHeader>
+            </Link>
+            {loginComponent()}
+        </Header>
     );
 };
 

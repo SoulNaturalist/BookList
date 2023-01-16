904ad8ea3042227dff44f92c7eeb298291bf5c0e
SoulNaturalist <omg12121@mail.ru>
2023-01-17 00:53:55 +0300

    Components CheckEmail and  ConfirmEmail rewritten to styled-components

--- front/src/components/CheckEmail.jsx
+++ front/src/components/CheckEmail.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
 import emailCheckImg from '../assets/undraw_Opened_re_i38e.png'
 
 function CheckEmail () {
+    const AlertTitle = styled.h2`
+    text-align: center;
+    position: relative;
+    top:120px;
+    font-family: 'Manrope', sans-serif;
+    `
+    const ImgEmail = styled.h2`
+    width:300px;
+    height:300px;
+    display: block;
+    margin:0 auto;
+    position: relative;
+    top:125px;
+    `
     return <div>
-        <h2 className="check_email">Проверьте почту,на неё было отправлено письмо с подтверждением.</h2>
-        <img src={emailCheckImg} alt="email" className="email_icon"/>
+        <AlertTitle>Проверьте почту,на неё было отправлено письмо с подтверждением.</AlertTitle>
+        <ImgEmail src={emailCheckImg} alt="email"/>
     </div>
 }
 export default CheckEmail;
--- front/src/components/ConfirmEmail.jsx
+++ front/src/components/ConfirmEmail.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import useSWR from 'swr';
+import styled from 'styled-components';
 import { useLocation } from "react-router-dom";
 import CircularProgress from '@mui/material/CircularProgress';
 
@@ -10,11 +11,25 @@ function ConfirmEmail () {
         credentials: 'include',
         body: JSON.stringify({code:sampleLocation.pathname.split("/")[2]})
   }).then(res => res.json()));
+  const TitleEmail = styled.h2`
+  text-align: center;
+  position: relative;
+  top:120px;
+  font-family: 'Manrope', sans-serif;
+  `
+  const ImgSuccess = styled.img`
+  width:300px;
+  height:300px;
+  display: block;
+  margin:0 auto;
+  position: relative;
+  top:125px;
+  `
   const emailComponent = () => {
     if (dataEmail['message'] === 'Почта потверждена!') {
       return <div>
-        <h2 className="email_confirm">Ваша почта подтверждена!<a href="http://127.0.0.1:3000/login">Зайти на аккаунт</a></h2>
-        <img src="https://www.suunto.com/contentassets/a831335e43eb4b3b90b55547ed33ab41/icon-success.png" className="icon_success" alt="icon_success"/>
+        <TitleEmail>Ваша почта подтверждена!<a href="http://127.0.0.1:3000/login">Зайти на аккаунт</a></TitleEmail>
+        <ImgSuccess src="https://www.suunto.com/contentassets/a831335e43eb4b3b90b55547ed33ab41/icon-success.png" alt="icon_success"/>
       </div>
     }
   }

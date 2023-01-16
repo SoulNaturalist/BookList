9070958f0efeb4df43d8dd0e384c66274736e60b
SoulNaturalist <omg12121@mail.ru>
2023-01-17 00:40:52 +0300

    Home rewritten to styled-components

--- front/src/components/Home.jsx
+++ front/src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import useSWR from 'swr';
+import styled from 'styled-components';
 import bookHomeImg from '../assets/undraw_Books_re_8gea.png'
 import CircularProgress from '@mui/material/CircularProgress';
 
@@ -15,16 +16,56 @@ function Home() {
             return "гость";
         }
     }
+    const ParagraphWelcome = styled.p`
+    font-size:25px;
+    text-align: center;
+    position:relative;
+    top:50px;
+    font-family: 'Manrope', sans-serif;
+    `
+    const ParagraphDescription = styled.p`
+    font-size:27px;
+    position: relative;
+    text-align: center;
+    top:80px;
+    font-family: 'Manrope', sans-serif;
+    `
+    const ImgLibrary = styled.img`
+    display:block;
+    margin:auto;
+    position: relative;
+    top:120px;
+    width:40%;
+    height:auto;
+    border-radius:10px;
+    border: 1px solid;
+    `
+    const WrapperLink = styled.div`
+    display:flex;
+    justify-content: center;
+    justify-items: center;
+    position: relative;
+    top:170px;
+    `
+    const LinkResource = styled.a`
+    margin-block:10px;
+    font-family: 'Manrope', sans-serif;
+    `
+    const Span = styled.span`
+    font-size:20px;
+    margin:5px;
+    font-family: 'Manrope', sans-serif;
+    `
     const libraryWelcomeElement = <div>
-        <p className="welcome_title">Добро пожаловать {Username()}</p>
-        <p className="site_description">BookList - это онлайн библиотека добавляй книги,пиши рецензии и оцени прочитанные книги по достоинству.</p>
-        <p className="site_description">Также ты можешь скачать книги на нашем сайте.</p>
-        <img className="library_photo" src={bookHomeImg} alt="library_photo"/>
-        <div className="rules_link">
-            <a href="/rules">Правила</a>
-            <span>&</span>
-            <a href="/catalog">Каталог</a>
-        </div>
+        <ParagraphWelcome>Добро пожаловать {Username()}</ParagraphWelcome>
+        <ParagraphDescription>BookList - это онлайн библиотека добавляй книги,пиши рецензии и оцени прочитанные книги по достоинству.</ParagraphDescription>
+        <ParagraphDescription>Также ты можешь скачать книги на нашем сайте.</ParagraphDescription>
+        <ImgLibrary src={bookHomeImg} alt="library_photo"/>
+        <WrapperLink>
+            <LinkResource href="/rules">Правила</LinkResource>
+            <Span>&</Span>
+            <LinkResource href="/catalog">Каталог</LinkResource>
+        </WrapperLink>
     </div>
     return (
         <div>

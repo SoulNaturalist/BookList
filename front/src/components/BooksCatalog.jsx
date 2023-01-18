8bd02dce23a06ae35de339e7f2dc6dcc56f5ecf1
SoulNaturalist <omg12121@mail.ru>
2023-01-18 23:29:46 +0300

    BooksCatalog rewritten to styled component

--- front/src/components/BooksCatalog.jsx
+++ front/src/components/BooksCatalog.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import useSWR from 'swr';
+import styled from 'styled-components';
 import CircularProgress from '@mui/material/CircularProgress';
 
 
@@ -31,36 +32,99 @@ function BooksCatalog () {
         credentials: 'include',
         body: JSON.stringify({text:Search})
     }).then(res => res.json()));
+    const TitleCatalog = styled.h1`
+    text-align: center;
+    margin-top:130px;
+    font-family: 'Manrope', sans-serif;
+    `
+    const SearchButton = styled.button`
+    all: unset;
+    cursor: pointer;
+    background-color:rgb(101, 245, 166);
+    width:5%;
+    height:40px;
+    text-align:center;
+    color:rgb(0, 0, 0);
+    position: relative;
+    top:85px;
+    left:71%;
+    font-family: 'Manrope';
+    box-shadow: "0px 5px 5px -5px rgba(34, 60, 80, 0.6)";
+    -webkit-box-shadow: 0px 5px 10px 2px rgba(143, 255, 146, 0.2);
+    -moz-box-shadow: 0px 5px 10px 2px rgba(143, 255, 146, 0.2);
+    box-shadow: 0px 5px 10px 2px rgba(143, 255, 146, 0.2);
+    `
+    const SearchInput = styled.input`
+    display: block;
+    position: relative;
+    top:40px;
+    margin-right: auto;
+    margin-left: auto;
+    color:#000;
+    width:40%;
+    border:2px solid #000;
+    padding: 10px 10px; 
+    line-height: 10px;
+    `
+    const BookCard = styled.div`
+    position: relative;
+    top:60px;
+    justify-items: center;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows:400px;
+    padding:15px;
+    flex-basis:50%;
+    `
+    const CoverBook = styled.img`
+    width:100%; 
+    height:auto;
+    `
+    const BookParagraph = styled.p`
+    margin-left:60px;
+    `
+    const HoverButton = styled.div`
+    &:hover ${SearchButton} {
+        background-color:rgb(255, 255, 255);
+      }
+    `
+    const FlexWrapper = styled.div`
+    display: flex;
+    justify-content:center;
+    position:relative;
+    top: 90px;
+    `
+
     const BooksCatalog = () => {
         return <div>
-            <h1 className="title__catalog">Каталог книг</h1>
-            <div className="flex-wrapper">
-            <button onClick={dataSearch} className="search_button">Найти</button>
-            <input type="search" className="search_books" defaultValue='Название книги' onChange={e => SearchData(e)}/>  
-            {!dataBooks ? <div style={{display: 'flex', justifyContent: 'center', position:'relative', top:'90px'}}><CircularProgress disableShrink /></div>:
+            <TitleCatalog>Каталог книг</TitleCatalog>
+            <HoverButton>
+                <SearchButton onClick={dataSearch}>Найти</SearchButton>
+            </HoverButton>
+            <SearchInput type="search" defaultValue='Название книги' onChange={e => SearchData(e)}/>  
+            {!dataBooks ? <FlexWrapper><CircularProgress disableShrink /></FlexWrapper>:
             Boolean(searchButton) ? (
                 dataBooks.map((book, index) => (
-                    <div key={index} className="book__card">
+                    <BookCard key={index}>
                         <a href={`/book/${book.slug}`}>
-                            <img src={book.cover} alt="cover" style={{ width: '100%', height:'auto'}}/>
-                            <p style={{marginLeft:'60px'}}>{book.book_name}</p> 
-                            <p style={{marginLeft:'60px'}}>{book.book_author}</p> 
+                            <CoverBook src={book.cover} alt="cover"/>
+                            <BookParagraph>{book.book_name}</BookParagraph> 
+                            <BookParagraph>{book.book_author}</BookParagraph> 
                         </a>
-                    </div>
+                    </BookCard>
                 ))
             ):(
                 dataSearch.map((book, index) => (
-                    <div key={index} className="book__card">
+                    <BookCard key={index}>
                         <a href={`/book/${book.slug}`}>
-                            <img src={book.cover} alt="cover" style={{ width: '100%', height:'auto'}}/>
-                            <p style={{marginLeft:'60px'}}>{book.book_name}</p> 
-                            <p style={{marginLeft:'60px'}}>{book.book_author}</p> 
+                            <CoverBook src={book.cover} alt="cover"/>
+                            <BookParagraph>{book.book_name}</BookParagraph> 
+                            <BookParagraph>{book.book_author}</BookParagraph> 
                         </a>
-                    </div>
+                    </BookCard>
                 ))
             )}
             </div>
-        </div>
     }
 
     return (

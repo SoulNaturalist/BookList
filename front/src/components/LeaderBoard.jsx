61cce84cd0b5a270de20edb52258ad8f2818d740
SoulNaturalist <omg12121@mail.ru>
2023-05-01 09:10:47 +0300

    LeaderBoard rewritten to useSWR

--- front/src/components/LeaderBoard.jsx
+++ front/src/components/LeaderBoard.jsx
@@ -1,54 +1,56 @@
 import React from 'react';
-import axios from "axios";
 import { styled } from '@mui/system';
 import ButtonUnstyled, { buttonUnstyledClasses } from '@mui/base/ButtonUnstyled';
-
-
+import useSWR from 'swr';
 
 function LeaderBoard () {
-    const [users, setUsers] = React.useState("");
     const [textTitle, setTextTitle] = React.useState("");
+    const [selectedLeader, setLeader] = React.useState("reviews");
+
     const CustomButtonRoot = styled('span')`
-    font-family: IBM Plex Sans, sans-serif;
-    font-weight: bold;
-    font-size: 0.875rem;
-    background-color: black;
-    padding: 18px 70px;
-    margin:20px;
-    border-radius:5px;
-    color: white;
-    transition: all 150ms ease;
-    cursor: pointer;
-    border: none;
-    &:hover {
-        background-color: #FF7A00;
-    }
-    &.${buttonUnstyledClasses.active} {
-        background-color: #ffb459;
-    }
-    &.${buttonUnstyledClasses.focusVisible} {
-        box-shadow: 0 4px 20px 0 rgba(61, 71, 82, 0.1), 0 0 0 5px rgba(0, 127, 255, 0.5);
-        outline: none;
-    }
+        font-family: IBM Plex Sans, sans-serif;
+        font-weight: bold;
+        font-size: 0.875rem;
+        background-color: black;
+        padding: 18px 70px;
+        margin:20px;
+        border-radius:5px;
+        color: white;
+        transition: all 150ms ease;
+        cursor: pointer;
+        border: none;
+        &:hover {
+            background-color: #FF7A00;
+        }
+        &.${buttonUnstyledClasses.active} {
+            background-color: #ffb459;
+        }
+        &.${buttonUnstyledClasses.focusVisible} {
+            box-shadow: 0 4px 20px 0 rgba(61, 71, 82, 0.1), 0 0 0 5px rgba(0, 127, 255, 0.5);
+            outline: none;
+        }
     `;
     function CustomButton(props) {
         return <ButtonUnstyled {...props} component={CustomButtonRoot} />;
     }
     const GetDataLeaders = (typeLeader) => {
-        if (typeLeader === "reviews") {
-            axios({method: 'post',url:`http://127.0.0.1:3030/api/get_leaders`,withCredentials: true, headers: {},data: {type_leaders:"reviews"}})
-            .then((res) => {
-                setUsers(res.data)
-                setTextTitle("Лучшие по количеству рецензий")
-            })
-        } else if (typeLeader === "books") {
-            axios({method: 'post',url:`http://127.0.0.1:3030/api/get_leaders`,withCredentials: true, headers: {},data: {type_leaders:"books"}})
-            .then((res) => {
-                setUsers(res.data)
-                setTextTitle("Лучшие по количеству книг")
-            })
-        }
-    }
+        const leaderObj = {"рецензий":"reviews", "симпатий":"likes", "книг":"books"};
+        setLeader(leaderObj[typeLeader]);
+        setTextTitle(`Лучшие по количеству ${typeLeader}`);
+    };
+
+    const { data: users } = useSWR(
+        'http://127.0.0.1:3030/api/get_leaders',
+        (url) =>
+          fetch(url, {
+            method: 'POST',
+            withCredentials: true,
+            headers: {},
+            body: JSON.stringify({ type_leaders: selectedLeader }),
+          }).then((res) => res.json()),
+          { revalidateOnFocus: true, refreshInterval: 500 },
+    );
+
     const leaderTable = () => {
         return <div className="leaders_table">
             <h3>{textTitle}</h3>
@@ -62,21 +64,19 @@ function LeaderBoard () {
             })}
         </div>
     }
-    
     return (
-            <div>
-                <div className="leader_title">
-                    <h2>Топ пользователей</h2>
-                </div>
-                <div className="leader_menu">
-                    <CustomButton onClick={() => GetDataLeaders("reviews")}>Рецензии</CustomButton>
-                    <CustomButton onClick={() => GetDataLeaders("books")}>Книги</CustomButton>
-                    <CustomButton onClick={() => GetDataLeaders("likes")}>Симпатии</CustomButton>
-                </div>
-                {users ? leaderTable():""}
+        <div>
+            <div className="leader_title">
+                <h2>Топ пользователей</h2>
             </div>
+            <div className="leader_menu">
+                <CustomButton onClick={() => GetDataLeaders("рецензий")}>Рецензии</CustomButton>
+                <CustomButton onClick={() => GetDataLeaders("книг")}>Книги</CustomButton>
+                <CustomButton onClick={() => GetDataLeaders("симпатий")}>Симпатии</CustomButton>
+            </div>
+            {users ? leaderTable() : ""}
+        </div>
     );
-    
 }
 
 export default LeaderBoard;

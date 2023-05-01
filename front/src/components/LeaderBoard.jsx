9e3e082ac29fea82d4439f635cf60219315eb3bd
SoulNaturalist <omg12121@mail.ru>
2023-05-01 09:34:55 +0300

    LeaderBoard css rewritten to styled-components

--- front/src/components/LeaderBoard.jsx
+++ front/src/components/LeaderBoard.jsx
@@ -1,38 +1,78 @@
 import React from 'react';
-import { styled } from '@mui/system';
-import ButtonUnstyled, { buttonUnstyledClasses } from '@mui/base/ButtonUnstyled';
+import styled from 'styled-components';
 import useSWR from 'swr';
 
 function LeaderBoard () {
     const [textTitle, setTextTitle] = React.useState("");
     const [selectedLeader, setLeader] = React.useState("reviews");
 
-    const CustomButtonRoot = styled('span')`
-        font-family: IBM Plex Sans, sans-serif;
-        font-weight: bold;
-        font-size: 0.875rem;
-        background-color: black;
-        padding: 18px 70px;
-        margin:20px;
-        border-radius:5px;
-        color: white;
-        transition: all 150ms ease;
-        cursor: pointer;
-        border: none;
-        &:hover {
-            background-color: #FF7A00;
-        }
-        &.${buttonUnstyledClasses.active} {
-            background-color: #ffb459;
-        }
-        &.${buttonUnstyledClasses.focusVisible} {
-            box-shadow: 0 4px 20px 0 rgba(61, 71, 82, 0.1), 0 0 0 5px rgba(0, 127, 255, 0.5);
-            outline: none;
-        }
-    `;
-    function CustomButton(props) {
-        return <ButtonUnstyled {...props} component={CustomButtonRoot} />;
+    const ButtonLeader = styled('span')`
+    font-family: IBM Plex Sans, sans-serif;
+    font-weight: bold;
+    font-size: 0.875rem;
+    background-color: black;
+    padding: 18px 70px;
+    margin:20px;
+    border-radius:5px;
+    color: white;
+    transition: all 150ms ease;
+    cursor: pointer;
+    border: none;
+    &:hover {
+        background-color: #FF7A00;
     }
+    &.active {
+        background-color: #ffb459;
+    }
+    &.focusVisible {
+         box-shadow: 0 4px 20px 0 rgba(61, 71, 82, 0.1), 0 0 0 5px rgba(0, 127, 255, 0.5);
+        outline: none;
+    }
+    `;
+    const LeaderTitle = styled('div')`
+    font-family: 'Manrope', sans-serif;
+    text-align: center;
+    position: relative;
+    top:50px;
+    `;
+    const LeaderMenu = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    top:100px;
+    `;
+    const LeaderUsers = styled.div`
+    background-color:rgb(255, 255, 255);
+    width:834px;
+    height:466px;
+    margin-left: auto;
+    margin-right: auto;
+    position: relative;
+    top:120px;
+    border-radius:10px;
+    -webkit-box-shadow: 0px 5px 10px 2px rgba(34, 60, 80, 0.2);
+    -moz-box-shadow: 0px 5px 10px 2px rgba(34, 60, 80, 0.2);
+    box-shadow: 0px 5px 10px 2px rgba(34, 60, 80, 0.2);
+    `;
+    const TitleLeaders = styled.h2`
+    font-family: 'Manrope', sans-serif;
+    text-align: center;
+    position: relative;
+    top:15px;
+    `;
+    const LeaderAvatar = styled.img`
+    border-radius:150px;
+    width:80px;
+    padding:20px;
+    display:block;
+    margin:auto;
+    `;
+    const LeaderUsername = styled.p`
+    position: relative;
+    text-align: center;
+    `;
+
     const GetDataLeaders = (typeLeader) => {
         const leaderObj = {"рецензий":"reviews", "симпатий":"likes", "книг":"books"};
         setLeader(leaderObj[typeLeader]);
@@ -52,28 +92,28 @@ function LeaderBoard () {
     );
 
     const leaderTable = () => {
-        return <div className="leaders_table">
-            <h3>{textTitle}</h3>
+        return <LeaderUsers>
+            <TitleLeaders>{textTitle}</TitleLeaders>
             {Object.values(users).map((user, index) => {
                 return <div key={index}>
                     <a href={`http://127.0.0.1:3000/user/${user.username}`}>
-                        <img src={user.avatar} alt="avatar" className="leader_avatar"/>
-                        <p className="leader_username">{user.username}</p>
+                        <LeaderAvatar src={user.avatar} alt="avatar"/>
+                        <LeaderUsername>{user.username}</LeaderUsername>
                     </a>
                 </div>
             })}
-        </div>
+        </LeaderUsers>
     }
     return (
         <div>
-            <div className="leader_title">
+            <LeaderTitle>
                 <h2>Топ пользователей</h2>
-            </div>
-            <div className="leader_menu">
-                <CustomButton onClick={() => GetDataLeaders("рецензий")}>Рецензии</CustomButton>
-                <CustomButton onClick={() => GetDataLeaders("книг")}>Книги</CustomButton>
-                <CustomButton onClick={() => GetDataLeaders("симпатий")}>Симпатии</CustomButton>
-            </div>
+            </LeaderTitle>
+            <LeaderMenu>
+                <ButtonLeader onClick={() => GetDataLeaders("рецензий")}>Рецензии</ButtonLeader>
+                <ButtonLeader onClick={() => GetDataLeaders("книг")}>Книги</ButtonLeader>
+                <ButtonLeader onClick={() => GetDataLeaders("симпатий")}>Симпатии</ButtonLeader>
+            </LeaderMenu>
             {users ? leaderTable() : ""}
         </div>
     );

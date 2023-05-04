640dd47f7533a354f38885911a25f86b4cb5e310
SoulNaturalist <omg12121@mail.ru>
2023-05-04 17:03:07 +0300

    rewritten UserReview to useSWR and created nginx directory

--- front/src/components/UserReview.jsx
+++ front/src/components/UserReview.jsx
@@ -1,40 +1,53 @@
-import React from 'react'
-import axios from "axios";
+import React from 'react';
 import Rating from '@mui/material/Rating';
-import {useParams, useNavigate} from "react-router-dom";
+import { useParams, useNavigate } from 'react-router-dom';
 import CircularProgress from '@mui/material/CircularProgress';
+import useSWR from 'swr';
+import axios from 'axios';
 
-export default function UserReview () {
-    const navigate = useNavigate("/");
-    const { username } = useParams();
-    const [Data,setData] = React.useState({});
-    const [user,setUser] = React.useState([]);
-    const [loading,setLoading] = React.useState(false);
-    React.useEffect(() => {
-        axios({method: 'post',url:'http://127.0.0.1:3030/api/auth',withCredentials: true, headers: {}})
-        .then(response => {
-            setData(response.data)
-        })
-    }, [])
-    React.useEffect(() => {
-        axios({method: 'get',url:`http://127.0.0.1:3030/api/user/${username}`,withCredentials: true, headers: {}})
-        .then(response => {
-            setUser(response.data[0])
-            setLoading(true)
-        })
-    }, [username])
-    return <div>
-        <h1 className="title_reviews_user">Рецензии {username}</h1>
-        {
-            Data ?
-            loading ? Object.keys(user.reviews).map((review,index) => (
-                <div key={index} className="review_card_profile">
-                    <h1 className="book_name_review">{review}</h1>
-                    <p className="book_title_review">{user.reviews[review].title}</p>
-                    <p className="book_description_review">{user.reviews[review].description}</p>
-                    <Rating name="read-only" value={user.reviews[review].rating} readOnly/>
-                </div>
-            )):<div style={{display: 'flex', justifyContent: 'center'}}><CircularProgress disableShrink /></div>:navigate("/login")
-        }
+export default function UserReview() {
+  const navigate = useNavigate();
+  const { username } = useParams();
+  const { data: authData } = useSWR('http://127.0.0.1:3030/api/auth', () =>
+    axios.get('http://127.0.0.1:3030/api/auth', { withCredentials: true })
+  );
+  const { data: userData } = useSWR(
+    username ? `http://127.0.0.1:3030/api/user/${username}` : null,
+    () =>
+      axios.get(`http://127.0.0.1:3030/api/user/${username}`, {
+        withCredentials: true,
+      })
+  );
+
+  if (!authData) {
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <CircularProgress disableShrink />
+      </div>
+    );
+  }
+
+  if (!userData) {
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <CircularProgress disableShrink />
+      </div>
+    );
+  }
+
+  const { reviews } = userData.data[0];
+
+  return (
+    <div>
+      <h1 className="title_reviews_user">Рецензии {username}</h1>
+      {Object.keys(reviews).map((review, index) => (
+        <div key={index} className="review_card_profile">
+          <h1 className="book_name_review">{review}</h1>
+          <p className="book_title_review">{reviews[review].title}</p>
+          <p className="book_description_review">{reviews[review].description}</p>
+          <Rating name="read-only" value={reviews[review].rating} readOnly />
         </div>
+      ))}
+    </div>
+  );
 }

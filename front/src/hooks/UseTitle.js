bf292d3c16f6b14e9ba248cbb92bc687c55e7231
SoulNaturalist <omg12121@mail.ru>
2023-09-23 23:44:12 +0300

    deleted meta tag

--- front/src/hooks/UseTitle.js
+++ front/src/hooks/UseTitle.js
@@ -6,7 +6,6 @@ export default function useTitle(props) {
     const title = props.title;
     const jsx = (
         <Helmet>
-            <meta charSet="utf-8"/>
             <title>{title}</title>
         </Helmet>
     );

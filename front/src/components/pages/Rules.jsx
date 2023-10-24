a8c487bf7c4d3bdb818d15f0b84fc597c0312f20
SoulNaturalist <omg12121@mail.ru>
2023-10-24 18:51:06 +0300

    fixed adaptive and added title to rules component

--- front/src/components/pages/Rules.jsx
+++ front/src/components/pages/Rules.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {FlexWrapper, WrapperRules, TitleRules, ParagraphRules, SubTitleRules} from "../styles/Rules.styles.js";
+import UseTitle from '../../hooks/UseTitle.js';
 
 function Rules () {
     return (
         <FlexWrapper>
+            <UseTitle title="Правила"></UseTitle>
             <WrapperRules>
                 <TitleRules>Правила BookList:</TitleRules>
                 <ParagraphRules>Пример правила 1</ParagraphRules>

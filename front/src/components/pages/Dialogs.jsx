b34243cf5539613076167042e68b2171c14f2d2c
SoulNaturalist <omg12121@mail.ru>
2024-03-14 22:47:05 +0300

    commented

--- front/src/components/pages/Dialogs.jsx
+++ front/src/components/pages/Dialogs.jsx
@@ -10,20 +10,20 @@ import {
 } from "../styles/Dialogs.styles.js";
 
 export default function Dialogs() {
-  const UsersDialog = [{"username":"SPD", "avatar":"https://cdn2.scratch.mit.edu/get_image/user/61770542_60x60.png"},
-  {"username":"SPD2", "avatar":"https://cdn2.scratch.mit.edu/get_image/user/61770542_60x60.png"},
-  {"username":"SPD3", "avatar":"https://cdn2.scratch.mit.edu/get_image/user/61770542_60x60.png"},
-  {"username":"SPD4", "avatar":"https://cdn2.scratch.mit.edu/get_image/user/61770542_60x60.png"},
-  {"username":"SPD5", "avatar":"https://cdn2.scratch.mit.edu/get_image/user/61770542_60x60.png"},
-  {"username":"SPD6", "avatar":"https://cdn2.scratch.mit.edu/get_image/user/61770542_60x60.png"},
-  {"username":"SPD7", "avatar":"https://cdn2.scratch.mit.edu/get_image/user/61770542_60x60.png"},
-  {"username":"SPD8", "avatar":"https://cdn2.scratch.mit.edu/get_image/user/61770542_60x60.png"},
-  {"username":"SPD9", "avatar":"https://cdn2.scratch.mit.edu/get_image/user/61770542_60x60.png"},
-  {"username":"SPD10", "avatar":"https://cdn2.scratch.mit.edu/get_image/user/61770542_60x60.png"},
-  ].map((user, index) =><div>
-      <Avatar src={user.avatar}/>
-      <DialogUsername>{user.username}</DialogUsername>
-  </div>)
+  // const UsersDialog = [{"username":"SPD", "avatar":"https://cdn2.scratch.mit.edu/get_image/user/61770542_60x60.png"},
+  // {"username":"SPD2", "avatar":"https://cdn2.scratch.mit.edu/get_image/user/61770542_60x60.png"},
+  // {"username":"SPD3", "avatar":"https://cdn2.scratch.mit.edu/get_image/user/61770542_60x60.png"},
+  // {"username":"SPD4", "avatar":"https://cdn2.scratch.mit.edu/get_image/user/61770542_60x60.png"},
+  // {"username":"SPD5", "avatar":"https://cdn2.scratch.mit.edu/get_image/user/61770542_60x60.png"},
+  // {"username":"SPD6", "avatar":"https://cdn2.scratch.mit.edu/get_image/user/61770542_60x60.png"},
+  // {"username":"SPD7", "avatar":"https://cdn2.scratch.mit.edu/get_image/user/61770542_60x60.png"},
+  // {"username":"SPD8", "avatar":"https://cdn2.scratch.mit.edu/get_image/user/61770542_60x60.png"},
+  // {"username":"SPD9", "avatar":"https://cdn2.scratch.mit.edu/get_image/user/61770542_60x60.png"},
+  // {"username":"SPD10", "avatar":"https://cdn2.scratch.mit.edu/get_image/user/61770542_60x60.png"},
+  // ].map((user, index) =><div>
+  //     <Avatar src={user.avatar}/>
+  //     <DialogUsername>{user.username}</DialogUsername>
+  // </div>)
   return (
     <div>
       <DialogsFrame></DialogsFrame>

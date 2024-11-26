24066acf17157ce8f3f3bf726fafa613190a07af
SoulNaturalist <omg12121@mail.ru>
2024-11-26 09:58:38 +0300

    update playwright

--- front/e2e/example.spec.js
+++ front/e2e/example.spec.js
@@ -1,14 +1,14 @@
 const { test, expect } = require('@playwright/test');
 
 test('has title', async ({ page }) => {
-  await page.goto('/');
+  await page.goto('http://localhost:3000/');
 
   await expect(page).toHaveTitle(/Добро\sпожаловать\s:\)/);
 });
 
 
 test('has title 2', async ({ page }) => {
-  await page.goto('/rules');
+  await page.goto('http://localhost:3000/rules');
 
   await expect(page).toHaveTitle(/Правила/);
 });
